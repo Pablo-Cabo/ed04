76f007e8b398abc3f2b4c182da9c51df910523eb
Pablo-Cabo <1548234@alu.murciaeduca.es>
2024-03-05 14:27:33 +0100

    PRIMER COMMIT

---
+++ src/main/java/org/example/Agenda.java
@@ -0,0 +1,94 @@
+package org.example;
+
+import java.util.*;
+
+/**
+ * Clase Agenda, representa un lista de contactos
+ * @see Contacto
+ */
+
+public class Agenda {
+
+    /**
+     * Variable que contiene los contactos
+     */
+
+    private List<Contacto> contacts; // Lista de Contacto
+
+    /**
+     * Constructor por defecto que inicializa la lista
+     */
+
+    public Agenda() {
+        this.contacts = new ArrayList<>();
+    }
+
+    /**
+     * Metodo para añadir contactos a la lista
+     * @param name Nombre del contacto
+     * @param phone Numero del contacto
+     */
+
+    public void addContact(String name, String phone) {
+        boolean exists = false;
+        for (Contacto c : contacts) {
+            if (c.getName().equalsIgnoreCase(name)) {
+                exists = true;
+                c.getPhones().add(phone);
+                break;
+            }
+        }
+
+        if (!exists) {
+            Contacto newContact = new Contacto(name, phone);
+            contacts.add(newContact);
+        }
+    }
+
+    /**
+     * Metodo para eliminar contactos de la lista
+     * @param name Nombre ddel contacto
+     */
+
+    public void removeContact(String name) {
+        Iterator<Contacto> it = contacts.iterator();
+
+        while (it.hasNext()) {
+            Contacto c = it.next();
+
+            if (c.getName().equalsIgnoreCase(name)) {
+                it.remove();
+            }
+        }
+    }
+
+    /**
+     * Metodo para modificar el numero de telefono del contacto
+     * @param name Nombre del contacto
+     * @param oldPhone Antiguo telefono del contacto
+     * @param newPhone Nuevo telefono del contacto
+     */
+
+    public void modifyPhoneNumber(String name, String oldPhone, String newPhone) {
+        for (Contacto c : contacts) {
+            if (c.getName().equalsIgnoreCase(name)) {
+                List<String> phones = c.getPhones();
+
+                int index = phones.indexOf(oldPhone);
+
+                if (index != -1) {
+                    phones.set(index, newPhone);
+                }
+            }
+        }
+    }
+
+    /**
+     * Metodo que devuelve los contactos de la lista
+     * @return Lista de contactos
+     */
+
+    public List<Contacto> getContacts() {
+        return this.contacts;
+    }
+}
---
+++ src/main/java/org/example/Contacto.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Clase Contacto, representa un contacto
+ */
+
+class Contacto {
+
+    /**
+     * Variable que almacena el nombre del contacto
+     */
+
+    private String name;
+
+    /**
+     * Variable que almacena el o los telefonos del contacto
+     */
+
+    private List<String> phones;
+
+    /**
+     * Constructor que inicializa los atributos de la clase
+     * @param name Nombre del contacto
+     * @param phone Telefono del contacto
+     */
+
+    public Contacto(String name, String phone) {
+        this.name = name;
+        this.phones = new ArrayList<>();
+        this.phones.add(phone);
+    }
+
+    /**
+     * Metodo para obtener el nombre del contacto
+     * @return Nombre del contacto
+     */
+
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Metodo para obtener el o los numeros del contacto
+     * @return Numero o numeros del contacto
+     */
+
+    public List<String> getPhones() {
+        return this.phones;
+    }
+}
